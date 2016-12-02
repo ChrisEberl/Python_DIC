b34964a90e9ac3b9799dff5d3cd89dd221987cba
Charlie Bourigault <bourigault.charlie@gmail.com>
2016-12-03 00:43:50 +0100

    Analysis Info
    Added Poisson Ratio plot in Analysis Info window.

--- interface/analysisInfos.py
+++ interface/analysisInfos.py
@@ -44,10 +44,14 @@ class analysisInfos(QDialog):
 
         filePath =  self.parent.fileDataPath+'/infoAnalysis.csv'
         filePathMarkers =  self.parent.fileDataPath+'/infoMarkers.csv'
+        strainX = self.parent.fileDataPath+'/strainx.csv'
+        strainY = self.parent.fileDataPath+'/strainy.csv'
         infos = getData.testReadFile(filePath, lib=1) #None if not found
         # 0 Name, 1 Reference Mode, 2 CorrSize, 3 nbProcesses, 4 total processing time, 5 nbImages, 6 nbMarkers, 7 nbImages * nbMarkers, 8 largeDisp YES/NO, 9 Author
         self.infos = np.char.decode(infos, encoding="ascii")
         self.markersInfos = getData.testReadFile(filePathMarkers) #None if not found
+        self.fileStrainX = getData.testReadFile(strainX) #None if not found
+        self.fileStrainY = getData.testReadFile(strainY) #None if not found
         if self.infos is None:
             missingFile = QLabel('infoAnalysis.csv file not found.')
             self.mainLayout.addWidget(missingFile)
@@ -192,7 +196,7 @@ class analysisInfos(QDialog):
         plotListLbl = QLabel('Display:')
         self.plotListBox = QComboBox()
         self.plotListBox.setMinimumWidth(200)
-        availablePlots = ['Correlation Errors', 'Plot2', 'Plot3']
+        availablePlots = ['Correlation Errors', 'Poisson Ratio']
         for plot in availablePlots:
             self.plotListBox.addItem(plot)
         self.plotListOptions = QComboBox()
@@ -231,6 +235,8 @@ class analysisInfos(QDialog):
         plotOptions = []
         if item == 0:
             plotOptions = ['All', 'Edge Area', 'Marker Out', 'NaN', 'No Std. Dev.', 'Outside SubPx.', 'Div. by 0', 'Low Corr.', 'Bad Peak']
+        if item == 1:
+            plotOptions = ['Compression/Extension along X', 'Compression/Extension along Y']
         for option in plotOptions:
             self.plotListOptions.addItem(option)
         self.plotInfos(0)
@@ -242,7 +248,9 @@ class analysisInfos(QDialog):
         onlyActives = self.plotListCheckBox.isChecked()
         activeImages = self.parent.analysisWidget.activeImages
         totalImages = self.parent.analysisWidget.nb_image
+
         if plotType == 0:
+            self.plotListCheckBox.setEnabled(True)
             errorList = []
             legend = []
             if onlyActives:
@@ -250,7 +258,6 @@ class analysisInfos(QDialog):
             else:
                 imageRange = totalImages
             if option == 0:
-
                 errors = np.unique(self.markersInfos)
                 for error in errors:
                     if error == 0:
@@ -296,4 +303,28 @@ class analysisInfos(QDialog):
                 ax = self.matplotlibPlot.matPlot
                 ax.text(.5, .5, 'No error.', ha='center', va='center', transform = ax.transAxes, color='red')
 
+        if plotType == 1:
+            self.plotListCheckBox.setEnabled(False)
+            nbImages = self.fileStrainX.shape[0]
+            nbInstances = self.fileStrainX.shape[1]
+            imageList = np.linspace(1, totalImages+1, totalImages, endpoint=False).astype(np.int)
+            poissonRatio = np.zeros((nbInstances, nbImages))
+            colors = ['blue', 'cornflowerblue', 'royalblue', 'navy']
+            if option > 0:
+                numerator = self.fileStrainX
+                denominator = self.fileStrainY
+            else:
+                numerator = self.fileStrainY
+                denominator = self.fileStrainX
+            for instance in range(nbInstances):
+                for image in range(len(activeImages)):
+                    if denominator[image, instance] != 0:
+                        poissonRatio[instance, image] = numerator[image, instance]/denominator[image, instance]
+                    else:
+                        poissonRatio[instance, image] = np.nan
+                clr = colors[instance % 4]
+                lbl = 'Instance '+str(instance)
+                self.matplotlibPlot.matPlot.plot(activeImages, poissonRatio[instance, :], '-', color=clr, label=lbl)
+                if nbInstances > 1:
+                    self.matplotlibPlot.matPlot.legend()
         self.matplotlibPlot.draw_idle()
